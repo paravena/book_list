beb7793646e23b82353adaaca0d7e615462c3409
paravena <paravena74@gmail.com>
2016-07-12 15:57:47 -0300

    first commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+  return [
+    {title : 'JavaScript the good parts'},
+    {title : 'Eloquent JavaScript'},
+    {title : 'Node.js in Action'},
+    {title : 'Spring in Action'},
+    {title : 'Functional Programming'}
+  ]
+}
