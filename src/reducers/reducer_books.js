7fea902a960b1be39517cc4f438ce2aafbb8eccc
paravena <paravena74@gmail.com>
2016-07-13 18:45:23 -0300

    redux integration completed

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,9 +1,9 @@
 export default function() {
   return [
-    {title : 'JavaScript the good parts'},
-    {title : 'Eloquent JavaScript'},
-    {title : 'Node.js in Action'},
-    {title : 'Spring in Action'},
-    {title : 'Functional Programming'}
+    {title : 'JavaScript the good parts', pages: 10},
+    {title : 'Eloquent JavaScript', pages: 229},
+    {title : 'Node.js in Action', pages: 67},
+    {title : 'Spring in Action', pages: 69},
+    {title : 'Functional Programming', pages: 100}
   ]
 }
